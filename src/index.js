b37c97216a2c362b7099f5b9fc4f7247872e10eb
at <metkraming@gmail.com>
2020-12-07 03:34:15 +0200

    let's start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>This is going to be a Draughts application on React Js</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
